9d25b655c9f14aaf8517d9ddf007f49c9eb57a46
shishir <ccrsharma7@gmail.com>
2021-11-12 10:17:09 +0100

    changes in the customer and main file

--- Customer.py
+++ Customer.py
@@ -1,42 +1,24 @@
 
 class Customer:
-def __init__(self,cur_pos,speed,PATH):
-        self.cur_pos = cur_pos
-        self.speed = 1
-        self.est_pos = (0,0)
+    def __init__(self, PATH):
+        self.cur_pos = PATH[0]
+        self.est_pos = (0, 0)
         self.PATH = PATH
         self._path_ind = 0
 
     def move(self):
         if len(self.PATH) + 1 == self._path_ind:
             return
-
-        if self.cur_pos == self.PATH[self._path_ind]:
-            self._path_ind += 1
-        
-        diff = [0,0]
-        diff[0] = self.cur_pos[0] - self.PATH[self._path_ind][0]
-        diff[1] = self.cur_pos[1] - self.PATH[self._path_ind][1]
-
-        if diff[0] > 0 and diff[1] > 0:
-            self.cur_pos[0] =+ self.speed/2
-            self.cur_pos[1] =+ self.speed/2
-
-        elif diff[0] > 0:
-            self.cur_pos[0] =+ self.speed
-        
-        elif diff[1] > 0:
-            self.cur_pos[1] =+ self.speed
-
+        self._path_ind += 1
+        self.cur_pos = self.PATH[self._path_ind]
 
 
 if __name__ == "__main__":
-    
 
-    PATH_1 = [(20,650),(80,650)]
-    cust1 = Customer(PATH_1[0],1,PATH_1)
+    PATH_1 = [(20, 650), (80, 650)]
+    cust1 = Customer(PATH_1)
 
-    for i in range(0,100):
+    for i in range(0, 100):
         cust1.move()
         print(cust1.cur_pos)
     # customers representing carts
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import time
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    i = 0;
+    i = 0
     # empty shop list
     shop = []
     # Calling object of Customer class
-    customer = Customer()
-    # Calling object of Path class
     path = Path()
+    customer = Customer(path)
+    # Calling object of Path class
 
 # loop for customer entering the shop
 while i < 5:
@@ -27,7 +27,8 @@ while i < 5:
     datetime1 = now.strftime("%H:%M:%S:%f %p")
     print("Customer with cart id {}, entered at {}".format(cart[0], datetime1))
     for j in range(5):
-        selectPath = random.choice(path.pathway[random.randrange(0, len(path.pathway))])
+        selectPath = random.choice(
+            path.pathway[random.randrange(0, len(path.pathway))])
         # shop.append(cart)
         time.sleep(random.randrange(0, 5))
         now = datetime.datetime.now()
@@ -41,5 +42,6 @@ while len(shop) > 0:
     time.sleep(random.randrange(0, 5))
     now = datetime.datetime.now()
     datetime1 = now.strftime("%H:%M:%S:%f %p")
-    print("Customer with cart id {} is at the checkout, at time {}".format(shop[0], datetime1))
+    print("Customer with cart id {} is at the checkout, at time {}".format(
+        shop[0], datetime1))
     shop.pop(0)
