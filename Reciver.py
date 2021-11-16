19c98e65fc8da8d347664f8738e34b70036527af
Bernhard <bernton96@gmail.com>
2021-11-16 14:55:28 +0100

    Working on overlap of signalswx

--- Reciver.py
+++ Reciver.py
@@ -3,33 +3,53 @@ import random as rand
 
 class Reciver:
     def __init__(self,pos_x,pos_y, Signal_radius):
-        self.Data_result = []
-        self.Pos = (pos_x,pos_y)
-        self._Signal_radius = Signal_radius
-        self._time_index = 0
+        self.Data_result = []               #hold recived signals
+        self.Pos = (pos_x,pos_y)            #Position of reciver in world
+        self._Signal_radius = Signal_radius #How far the signal can be recived
+        self._time_index = 0                #Index for data array logic 
 
-    def append_signal(self, cust_id, cust_pos, ts,curr_time):
-        print(self._time_index)
-        print(curr_time)
-        print(self.Data_result)
-        tmp = [cust_id]
-        if self._distance(cust_pos) <= self._Signal_radius and self.Data_result[self._time_index-1] == 0:
-            self.Data_result += tmp 
+    def append_signal(self, cust_id, cust_pos):
+        """
+        This function addes the recived signals to the data array of the
+        reciver
+            cust_id = id of signal from customer, i.e. customer 1 has id 1
+            cust_pos = position of customer when sending signal
+        """
+        
+        tmp = [cust_id] #The signal id
+        if len(self.Data_result) == 0: #if first signal to be recived
+            self.Data_result += tmp
+
+        #Add signal to array if signal in signal radius
+        elif self._distance(cust_pos) <= self._Signal_radius:
+            if len(self.Data_result) == self._time_index:
+                self.Data_result += tmp
+            else:
+                print("OVERLAP IN SIGNAL")
+                self.Data_result[self._time_index] = 0  
             
-
 
     def clear_signal(self, ts_1, ts_2):
         pass
 
-    def _distance(self, cust_pos):
+    def _distance(self, cust_pos): 
+        """
+        This function calculates the distance the signal propigates 
+        using pythagoran formula
+        """
         return np.sqrt((cust_pos[0] - self.Pos[0])**2 + (cust_pos[1] - self.Pos[1])**2)
     
     def update(self):
-        if self._time_index <= len(self.Data_result):
+        """ 
+        This function updates the time index of the reciver.
+        Needs to be called at the end of  every time interval
+        """
+        if self._time_index < len(self.Data_result):
             self._time_index += 1
         else:
             self.Data_result.append(0)
             self._time_index += 1
+
 
 def Update_recivers(Recivers):
     for i in  range(len(Recivers)):
@@ -45,38 +65,33 @@ def Init_Recivers(N,ts):
 if __name__ == "__main__":
     
     rand.seed(2)
-    N = 3
-    SIGNAL_RATE = 8
-    SIGNAL_TIME = 3
-    SIGNAL_STRENGHT = 100
+    N = 1
+    SIGNAL_RATE = 8 #How long time passes between signals
+    SIGNAL_TIME = 3 #How long each signal lasts, transmission time
+    SIGNAL_STRENGHT = 100 #Radius of reciver signaling space
 
 
     Recivers = Init_Recivers(N,SIGNAL_STRENGHT)
-    print(Recivers[0].Pos)
-    print(Recivers[1].Pos)
-    cust1_signal_rate = 0
+    cust1_signal_rate = 2
     cust1_signaling = 0
 
-    cust2_signal_rate = 2
+    cust2_signal_rate = 3
     cust2_signaling = 0
     
     for i in range(0,20):
         
-        Update_recivers(Recivers)
         print(i)
         if cust1_signal_rate == 0 or cust1_signaling > 0:
             print("CUST 1 SIGNALING")
-            Recivers[0].append_signal(1,(2,3),SIGNAL_TIME,i)
+            Recivers[0].append_signal(1,(2,3))
             cust1_signaling = (cust1_signaling + 1) % SIGNAL_TIME
-        """
-        if cust2_signal_rate == 0 or cust1_signaling > 0:
+        
+        if cust2_signal_rate == 0 or cust2_signaling > 0:
             print("CUST 2 SIGNALING")
-            Recivers[0].append_signal(2,(2,3),SIGNAL_TIME,i)
+            Recivers[0].append_signal(2,(2,3))
             cust2_signaling = (cust2_signaling + 1) % SIGNAL_TIME
-       """ 
-        print(Recivers[0].Data_result)
+        Update_recivers(Recivers)
         cust1_signal_rate = (cust1_signal_rate + 1) % SIGNAL_RATE
         cust2_signal_rate = (cust2_signal_rate + 1) % SIGNAL_RATE
     print(Recivers[0].Data_result)
         
-    
